86f85511477ead1583defb61a79911c645c1755e
deeware <55303607+deeware@users.noreply.github.com>
2021-06-29 20:13:13 +0530

    Update settings.py

--- BlogAware/settings.py
+++ BlogAware/settings.py
@@ -152,8 +152,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "blogaware.dee@gmail.com"
-EMAIL_HOST_PASSWORD = "Blog Anna"
+EMAIL_HOST_USER = "********************************"
+EMAIL_HOST_PASSWORD = "********"
 
 
 
